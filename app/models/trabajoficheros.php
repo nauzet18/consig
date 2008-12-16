622104c6b6ac73d6b6ad5b10dc99c2bd276311bb
JORGE LOPEZ PEREZ <jorgelp@us.es>
2008-12-16 12:57:23 +0000

    Ups, los comentarios sobre AGPL tenían referencias a Foobar

--- app/models/trabajoficheros.php
+++ app/models/trabajoficheros.php
@@ -4,18 +4,18 @@
  *
  *    This file is part of Consigna.
  *
- *    Foobar is free software: you can redistribute it and/or modify it
+ *    Consigna is free software: you can redistribute it and/or modify it
  *    under the terms of the GNU Affero General Public License as
  *    published by the Free Software Foundation, either version 3 of the
  *    License, or (at your option) any later version.
  *
- *    Foobar is distributed in the hope that it will be useful, but
+ *    Consigna is distributed in the hope that it will be useful, but
  *    WITHOUT ANY WARRANTY; without even the implied warranty of
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  *    Affero General Public License for more details.
  *
  *    You should have received a copy of the GNU Affero General Public
- *    License along with Foobar.  If not, see
+ *    License along with Consigna.  If not, see
  *    <http://www.gnu.org/licenses/>.
  */
 

89ae52315489200dcff780e4f27440cbc7b173b1
JORGE LOPEZ PEREZ <jorgelp@us.es>
2009-03-19 22:28:27 +0000

    Correcciones varias

--- mysql-database/bloque.sql
+++ mysql-database/bloque.sql
@@ -45,3 +45,12 @@ CREATE TABLE IF NOT EXISTS `cacheldap` (
 	`timestamp` INT UNSIGNED NOT NULL,
 
 	PRIMARY KEY(dn));
+
+CREATE TABLE IF NOT EXISTS  `ci_sessions` (
+	session_id varchar(40) DEFAULT '0' NOT NULL,
+	ip_address varchar(16) DEFAULT '0' NOT NULL,
+	user_agent varchar(50) NOT NULL,
+	last_activity int(10) unsigned DEFAULT 0 NOT NULL,
+	user_data text NOT NULL,
+	PRIMARY KEY (session_id)
+);

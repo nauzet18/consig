f1e24f5790cf496610d60c9c8a2a87beb271e8c3
JORGE LOPEZ PEREZ <jorgelp@us.es>
2009-04-10 17:09:50 +0000

    * Documentación variada * En el correo de expiración de un fichero se entrecomilla el nombre

--- app/views/email-fichero-caducado.php
+++ app/views/email-fichero-caducado.php
@@ -1,4 +1,4 @@
-El fichero <?php echo $nombre_fichero ?> que depositó en el servicio de
+El fichero "<?php echo $nombre_fichero ?>" que depositó en el servicio de
 consigna de la Universidad de Sevilla ha caducado. A partir de este momento
 no estará disponible para su descarga.
 

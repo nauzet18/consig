87898c467737e6fd818a4d4d541aeb920ff8d365
JORGE LOPEZ PEREZ <jorgelp@us.es>
2009-04-22 09:41:33 +0000

    Dirección de contacto (ofuscada) en el pie de página

--- app/views/pie.php
+++ app/views/pie.php
@@ -3,7 +3,10 @@
 <div id="pie">
  <a href="http://www.us.es/servicios/sic">Servicio de Inform&aacute;tica
  y Comunicaciones</a><br />
- <a href="http://www.us.es">Universidad de Sevilla</a>
+ <a href="http://www.us.es">Universidad de Sevilla</a><br />
+
+ <a class="dircorreo"
+ href='m&#97;ilt&#111;&#58;con&#37;7&#51;i%6&#55;n&#37;6&#49;&#64;u%73&#46;%&#54;&#53;s'>consigna&#64;&#117;&#115;&#46;es</a>
 
  <p id="version_consigna">
   consigna v1<span>.</span>1

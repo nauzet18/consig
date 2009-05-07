e0fbf6725c32b09fc00d1821af04e9a75dd86274
JORGE LOPEZ PEREZ <jorgelp@us.es>
2009-05-07 10:25:40 +0000

    Hacemos uso de la biblioteca OBSession para CI. Ésta corrige el comportamiento errático de la original de CI, que presenta además algunos problemas (guarda datos en la cookie del usuario!).

--- mysql-database/bloque.sql
+++ mysql-database/bloque.sql
@@ -28,14 +28,6 @@ CREATE TABLE IF NOT EXISTS `mimetypes` (
 
 	PRIMARY KEY(mid));
 
-CREATE TABLE IF NOT EXISTS  `ci_sessions` (
-	session_id varchar(40) DEFAULT '0' NOT NULL,
-	ip_address varchar(16) DEFAULT '0' NOT NULL,
-	user_agent varchar(50) NOT NULL,
-	last_activity int(10) unsigned DEFAULT 0 NOT NULL,
-	user_data text NOT NULL,
-	PRIMARY KEY (session_id)
-);
 
 CREATE TABLE IF NOT EXISTS `usercache` (
 	`id` VARCHAR(200) NOT NULL,
@@ -45,11 +37,12 @@ CREATE TABLE IF NOT EXISTS `usercache` (
 
 	PRIMARY KEY(id));
 
-CREATE TABLE IF NOT EXISTS  `ci_sessions` (
-	session_id varchar(40) DEFAULT '0' NOT NULL,
-	ip_address varchar(16) DEFAULT '0' NOT NULL,
-	user_agent varchar(50) NOT NULL,
-	last_activity int(10) unsigned DEFAULT 0 NOT NULL,
-	user_data text NOT NULL,
-	PRIMARY KEY (session_id)
-);
+CREATE TABLE IF NOT EXISTS `ci_sessions` (
+session_id varchar(40) DEFAULT '0' NOT NULL,
+session_start int(10) unsigned DEFAULT 0 NOT NULL,
+session_last_activity int(10) unsigned DEFAULT 0 NOT NULL,
+session_ip_address varchar(16) DEFAULT '0' NOT NULL,
+session_user_agent varchar(50) NOT NULL,
+session_data text default '' NOT NULL,
+PRIMARY KEY (session_id)
+); 

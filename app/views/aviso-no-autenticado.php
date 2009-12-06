ca5066705c524350a83e6ca25fc1417105bd10a0
JORGE LOPEZ PEREZ <jorgelp@us.es>
2009-12-06 12:42:41 +0000

    Separo las opciones avanzadas de las opciones de configuración propias de consigna

--- app/views/aviso-no-autenticado.php
+++ app/views/aviso-no-autenticado.php
@@ -1,5 +1,5 @@
  <div id="aviso">
-  Puede utilizar su nombre de usuario (UVUS) y contraseña de la
-  Universidad de Sevilla para tener acceso a todos los ficheros, además
-  de poder configurar y controlar sus envíos
+  Para tener acceso a todos los ficheros, además de poder configurar y controlar
+  sus envíos es necesario que se identifique utilizando la opción
+  <em>Autenticación</em>
  </div>

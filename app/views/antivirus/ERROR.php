7dcfdbf23109fc8dbd932924415fb192f2093b5f
JORGE LOPEZ PEREZ <jorgelp@us.es>
2010-04-20 20:06:15 +0000

    Los administradores ven los mensajes de error del antivirus

--- app/views/antivirus/ERROR.php
+++ app/views/antivirus/ERROR.php
@@ -1,3 +1,9 @@
 <div id="info_av" class="averror">
 El fichero no pudo ser analizado por el antivirus.
+<?php if ($this->gestionpermisos->es_privilegiado()):
+	?>
+	<div class="avextra"><?php echo $extra ?></div>
+	<?php
+endif;
+?>
 </div>

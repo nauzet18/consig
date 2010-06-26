c5dac7f0cd16fd66f684a0a1e8b9d3fc5b640b9f
JORGE LOPEZ PEREZ <jorgelp@us.es>
2010-06-26 12:40:14 +0000

    Los errores de aplicación (no 404 de CodeIgniter) ahora usan site_url() y no URLs absolutas

--- app/errors/error_general.php
+++ app/errors/error_general.php
@@ -7,7 +7,7 @@
 
 <head>
 <title>Consigna | algo fue mal</title>
-<link rel="stylesheet" href="/css/estilo.css" type="text/css" media="screen"
+<link rel="stylesheet" href="<?php echo site_url('css/estilo.css')?>" type="text/css" media="screen"
 />
 
 </head>
@@ -17,23 +17,23 @@
 <div id="cabecera">
  <div id="logo">
   <a href="http://www.us.es">
-     <img id="imagen" src="/img/logos/Logo.gif" 
+     <img id="imagen" src="<?php echo site_url('img/logos/Logo.gif')?>" 
   	alt="Universidad de Sevilla" />
   </a>
   <div id="titulologo">
-  <h1><a href="/">Consigna</a></h1>
+  <h1><a href="<?php echo site_url()?>">Consigna</a></h1>
   <h2 id="subtitulo">Env&iacute;o y recogida de ficheros</h2>
   </div>
  </div> <!-- logo -->
 </div>
 
 <div id="contenido">
-       <h1><?php echo $heading; ?></h1>
+       <h1>Hubo un error</h1>
 	   <div class="error">
 	   <?php echo $message; ?>
 	   </div>
 </div>
 
-
 </body>
+</html>
 

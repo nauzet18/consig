f35439b1ea654b83ffa09bfd996c68ca44f1a86d
JORGE LOPEZ PEREZ <jorgelp@us.es>
2010-06-27 08:32:55 +0000

    Mejora del script de migración

--- extra/av_poner_todos_pendientes.sh
+++ extra/av_poner_todos_pendientes.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #
-# Lee la tabla de ficheros, y los añade todos con estado PENDIENTE en la
-# tabla de antivirus. En la siguiente llamada a '/cron' se encolarán
-# para su análisis.
+# Lee las tablas de ficheros y antivirus, y todos los ficheros que no estén 
+# en la tabla antivirus los añade con estado PENDIENTE y timestamp 0. 
+#
+# En la siguiente llamada a '/cron' se encolarán para su análisis.
 
 UMYSQL=$1
 PMYSQL=$2
 TMYSQL=$3
 
-echo "SELECT fid FROM ficheros"| mysql \
+echo "SELECT ficheros.fid FROM ficheros LEFT OUTER JOIN antivirus ON ficheros.fid = antivirus.fid WHERE antivirus.estado IS NULL"| mysql \
 	--batch --skip-column-names -u${UMYSQL} -p${PMYSQL} $TMYSQL | while read i; do
 
 	echo "INSERT INTO antivirus(fid,estado,timestamp) VALUES ('${i}', 'PENDIENTE', '0');"

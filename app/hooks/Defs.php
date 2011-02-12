39f40bdfa09e8b49d2cb06cbc5a32561098883a4
JORGE LOPEZ PEREZ <jorgelp@us.es>
2011-02-12 12:42:13 +0000

    Añado el hook para la definición de la versión

---
+++ app/hooks/Defs.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * Copyright 2011 Jorge López Pérez <jorgelp@us.es>
+ *
+ *    This file is part of Consigna.
+ *
+ *    Consigna is free software: you can redistribute it and/or modify it
+ *    under the terms of the GNU Affero General Public License as
+ *    published by the Free Software Foundation, either version 3 of the
+ *    License, or (at your option) any later version.
+ *
+ *    Consigna is distributed in the hope that it will be useful, but
+ *    WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *    Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public
+ *    License along with Consigna.  If not, see
+ *    <http://www.gnu.org/licenses/>.
+ */
+
+class Defs {
+	function definiciones() {
+		define('VERSIONCONSIGNA', '1.4');
+	}
+}

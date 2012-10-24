be3ad0b79c106acb5480f60eadd9b8fda49c6e85
Jorge López Pérez <jorge@adobo.org>
2012-10-24 08:57:57 +0200

    Actualiza libopensso-php

--- app/libraries/authmodules/libopensso-php/config.php
+++ app/libraries/authmodules/libopensso-php/config.php
@@ -1,14 +1,7 @@
 <?php
+// Configuration package used
+define('CONFIG_PACK', 'pre_11');
 
-define('OPENSSO_BASE_URL', 'https://opensso-pre.us.es/opensso/');
-define('OPENSSO_COOKIE_NAME', 'iPlanetDirectoryPro');
-define('OPENSSO_LOGIN_URL',      OPENSSO_BASE_URL . 'UI/Login');
-define('OPENSSO_LOGOUT_URL',     OPENSSO_BASE_URL . 'UI/Logout');
-define('OPENSSO_LOGOUT_SERVICE',     OPENSSO_BASE_URL . 'identity/logout');
-define('OPENSSO_IS_TOKEN_VALID', OPENSSO_BASE_URL .
-		'identity/isTokenValid');
-define('OPENSSO_ATTRIBUTES',     OPENSSO_BASE_URL . 'identity/attributes');
-define('OPENSSO_COOKIE_NAME_FETCH',     OPENSSO_BASE_URL .
-		'identity/getCookieNameForToken');
-define('OPENSSO_DOMAIN', ".us.es");
-?>
+
+// Package directory (do not change)
+define('PACK_DIR', dirname(__FILE__) . '/packs/');
